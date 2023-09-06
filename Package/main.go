8cb5bd7d4c3353bc976d0db56a1bba7c375e6bb0
GovindSingh9447 <govindkrsingh0605@gmail.com>
2023-09-07 02:13:18 +0530

    package

---
+++ Package/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+
+	fmt.Printf("Hello %T %v", 10, 20 )
+	fmt.Println()
+	fmt.Printf("Number: %b", 1234)
+	fmt.Println()
+	fmt.Printf("Number: %o", 10008)
+	fmt.Println()
+	fmt.Printf("Number: %d", 111001)
+	fmt.Println()
+	fmt.Printf("Number: %e", 11.100145545)
+	fmt.Println()
+	fmt.Printf("Number: %f", 5.134541001)
+	fmt.Println()
+	fmt.Printf("Number: %g", 112435611001)
+
+
+
+
+}
