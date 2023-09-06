1655a12682de439220e961bdf98f8ec95d5076c4
GovindSingh9447 <govindkrsingh0605@gmail.com>
2023-09-07 01:52:08 +0530

    Variable

---
+++ Variable-&-Data-Type/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var name string ="Hello Bharat";
+	var number uint32 = 1947
+	fmt.Println(name," ",number)
+
+
+	//implicit variable
+
+	var numbers = 200023.47
+	fmt.Println(numbers)
+	fmt.Println("%T", numbers)
+
+
+	var b1 bool
+	b1=true
+	fmt.Println(number, b1)
+}
+
+
